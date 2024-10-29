edd82b7b812ceb0ab43f06a99c65d1f1206fea2c
ExistentialistMaverick <juliuslipscomb@gmail.com>
2024-10-28 22:51:50 -0400

    FinalQuoteMachine

--- quote-machine/src/App.jsx
+++ quote-machine/src/App.jsx
@@ -1,32 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import quotesBank from './assets/QuoteData.js'
-
+import { useState } from 'react';
+import './App.css';
+import quotesBank from './assets/QuoteData.js';
 
 function App() {
-
-  const newQuote = quotesBank[Math.floor(Math.random() * quotesBank.length)]
-  
   const [quote, setQuote] = useState('');
-  
-  const getNewQuote = () => setQuote(newQuote);
-  
+  const [author, setAuthor] = useState('');
+
+  const getNewQuote = () => {
+    const newQuote = quotesBank[Math.floor(Math.random() * quotesBank.length)];
+    setQuote(newQuote.quote);
+    setAuthor(newQuote.author);
+  };
+
+  // Generate a quote when the component mounts
+  useState(() => {
+    getNewQuote();
+  }, []);
+
+  const tweetQuote = `https://twitter.com/intent/tweet?text=${encodeURIComponent(quote)} - ${encodeURIComponent(author)}`;
 
   return (
     <main id="main-form" className='main-form'>
       <div id="quote-box" className="quote-container">
         <h1>Quote Machine</h1>
-        <p id="text" className='quote'>{newQuote.quote}</p>
-        <p id="author" className='author'>- {newQuote.author}</p>
+        <p id="text" className='quote'>{quote}</p>
+        <p id="author" className='author'>- {author}</p>
         <div id="actions" className='actions'>
           <button id="new-quote" onClick={getNewQuote}>New Quote</button>
-          <a id="tweet-quote" target="_blank" href='twitter.com/intent/tweet'>Tweet Quote</a>
+          <a id="tweet-quote" target="_blank" rel="noopener noreferrer" href={tweetQuote}>Tweet Quote</a>
         </div>
       </div>
     </main>
   );
 }
 
-export default App
+export default App;
